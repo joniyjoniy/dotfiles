7df59f08e1dab175666def5d6608f13cbe5648dd
joniyjoniy <joniyojoniylo@gmail.com>
2015-04-17 16:55:13 +0900

    edit symlink

--- symlink.sh
+++ symlink.sh
@@ -1,4 +1,14 @@
 #!/bin/zsh
 
 ln -snf ~/dotfiles/zsh/.zshrc ~/
-ln -s ~/dotfiles/emacs.d ~/.emacs.d
+ln -snf ~/dotfiles/.tmux.conf ~/
+
+#ln -s ~/dotfiles/emacs.d ~/.emacs.d
+dir=~/.emacs.d/
+
+if [ ! -e $dir ]; then
+    mkdir $dir
+    echo "mkdir $dir"
+fi
+
+ln -snf ~/dotfiles/emacs.d/init.el $dir
