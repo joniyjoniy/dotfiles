1badebd887b072bbbd34d4a1eea18c6a2d0ecf6e
joniyjoniy <s13t264@stmail.eng.kagawa-u.ac.jp>
2015-07-22 01:47:03 +0900

    add use-package

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -2,12 +2,12 @@
 ;;packege.el
 ;;-------------------------------------------------------------------------------------
 (require 'package)
+;; 初期化
+(package-initialize)
 ;; MELPAを追加
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 ;; Marmaladeを追加
 (add-to-list 'package-archives  '("marmalade" . "http://marmalade-repo.org/packages/"))
-;; 初期化
-(package-initialize)
 
 ;;-------------------------------------------------------------------------------------
 ;; cask
--- emacs.d/inits/10_packages.el
+++ emacs.d/inits/10_packages.el
@@ -1,9 +1,14 @@
 ;;-------------------------------------------------------------------------------------
+;; use-package
+;;-------------------------------------------------------------------------------------
+(require 'use-package)
+
+;;-------------------------------------------------------------------------------------
 ;;helm
 ;;-------------------------------------------------------------------------------------
 (add-to-list 'load-path "~/.emacs.d/elisp/helm")
 (global-set-key (kbd "C-x C-b") 'helm-mini)
-(require 'helm-config)
+(use-package helm-config)
 (helm-mode t)
 ;; M-yでキルリングの履歴一覧を表示
 (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
@@ -12,14 +17,13 @@
 ;; tabでディレクトリ移動
 (define-key helm-read-file-map (kbd "TAB") 'helm-execute-persistent-action)
 
-
 ;;-------------------------------------------------------------------------------------
 ;; auto-complete
 ;;-------------------------------------------------------------------------------------
-(require 'auto-complete)
+(use-package auto-complete)
 (global-auto-complete-mode t)
-(require 'auto-complete-config)
-(require 'fuzzy)
+(use-package auto-complete-config)
+(use-package fuzzy)
 (ac-config-default)
 ;; C-n/C-pで候補選択
 (setq ac-use-menu-map t)
@@ -29,18 +33,25 @@
 (define-key ac-mode-map (kbd "C-;") 'ac-fuzzy-complete)
 (add-to-list 'ac-modes 'slim-mode 'rhtml-mode)
 
-
 ;;-------------------------------------------------------------------------------------
 ;; ruby
 ;;-------------------------------------------------------------------------------------
-(autoload 'ruby-mode "ruby-mode"
-  "Mode for editing ruby source files" t)
-;; .rb Capfile GemfileをRubyファイルと認識
-(add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
-(add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
-(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+(use-package 'ruby-mode
+  :commands ruby-mode
+  :mode (("\\.rb$latex" . ruby-mode)
+         ("Capfile$" . ruby-mode)
+         ("Gemfile$" . ruby-mode))
+  :interpreter "ruby")
+
+;; (autoload 'ruby-mode "ruby-mode"
+;;   "Mode for editing ruby source files" t)
+;; ;; .rb Capfile GemfileをRubyファイルと認識
+;; (add-to-list 'auto-mode-alist '("\\.rb$latex " . ruby-mode))
+;; (add-to-list 'auto-mode-alist '("Capfile$" . ruby-mode))
+;; (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
+
 ;; ruby-electric
-(require 'ruby-electric)
+(use-package ruby-electric)
 (add-hook 'ruby-mode-hook '(lambda () (ruby-electric-mode t)))
 (setq ruby-electric-expand-delimiters-list nil)
 ;; end対策
@@ -50,30 +61,29 @@
   (ruby-indent-line t)
   (end-of-line))
 ;; ruby-block
-(require 'ruby-block)
+(use-package ruby-block)
 (ruby-block-mode t)
 (setq ruby-block-heilight-toggle t)
-
 
 ;;-------------------------------------------------------------------------------------
 ;; rails
 ;;-------------------------------------------------------------------------------------
-(require 'rhtml-mode)
-(require 'coffee-mode)
+(use-package rhtml-mode)
+(use-package coffee-mode)
 ;; projective-rails
-(require 'projectile)
+(use-package projectile)
 (projectile-global-mode)
-(require 'projectile-rails)
+(use-package projectile-rails)
 (add-hook 'projectile-mode-hook 'projectile-rails-on)
 (setq echo-keystrokes 0.1)
 (setq projectile-completion-system 'grizzl)
-(require 'recentf-ext)
+(use-package recentf-ext)
 (setq recentf-max-saved-items 5000)
 
 ;;-------------------------------------------------------------------------------------
 ;; web-mode
 ;;-------------------------------------------------------------------------------------
-(require 'web-mode)
+(use-package web-mode)
 ;; web-modeの設定
 (defun web-mode-hook ()
   (setq web-mode-markup-indent-offset 2)
@@ -87,21 +97,21 @@
 ;;-------------------------------------------------------------------------------------
 ;; lua-mode
 ;;-------------------------------------------------------------------------------------
-(require 'lua-mode)
+(use-package lua-mode)
 ;;lua-modeの設定
 (setq lua-indent-level 2)
 
 ;;-------------------------------------------------------------------------------------
 ;; twitter
 ;;-------------------------------------------------------------------------------------
-;; (require 'twittering-mode)
+;; (use-package twittering-mode)
 ;; (setq twittering-)
 
 ;;-------------------------------------------------------------------------------------
 ;; snippet
 ;;-------------------------------------------------------------------------------------
 ;; (add-to-list 'load-path "~/.emacs.d/elisp/yasnippet")
-;; (require 'yasnippet)
+;; (use-package yasnippet)
 ;; (setq yas-snippet-dirs '("~/.emacs.d/snippets"))
 ;; (yas-global-mode 1)
 ;; 既存スニペットを挿入する
