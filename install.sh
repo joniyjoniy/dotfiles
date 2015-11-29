a6804f71aac3e3ca3d1c30b16a7a0969a788be5e
joniyjoniy <joniyjoniylo@gmail.com>
2015-11-29 09:33:18 +0000

    cask add php lua

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#1/local/zsh
+#!/local/bash
 
 curl -O http://core.ring.gr.jp/pub/GNU/emacs/emacs-24.5.tar.gz
 tar -zxf emacs-24.5.tar.gz
@@ -6,3 +6,5 @@ cd emacs-24.5
 ./configure --without-x
 make
 sudo make install
+
+curl -fsSkL https://raw.github.com/cask/cask/master/go | python
