a39c5653d40e78eb295db868a586a117b2edd9e1
joniyjoniy <joniyjoniylo@gmail.com>
2015-12-09 10:17:28 +0000

    [add] cask.sh

---
+++ cask.sh
@@ -0,0 +1,3 @@
+# cask
+curl -fsSkL https://raw.github.com/cask/cask/master/go | python
+echo 'export PATH="$PATH":~/.cask/bin' >> ~/.zshenv
--- install.sh
+++ install.sh
@@ -13,9 +13,5 @@ cd emacs-24.5
 make
 sudo make install
 
-# cask
-curl -fsSkL https://raw.github.com/cask/cask/master/go | python
-echo 'export PATH="$PATH":~/.cask/bin' >> ~/.zshenv
-
 # tmux
 sudo apt-get install tmux
